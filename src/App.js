ac11d3ad3af947f0b2d509234dbb06a21b093e89
mona <mo.na.di.87.de@gmail.com>
2023-05-21 00:02:38 +0300

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather app
+      <h1> Weather app</h1>
+      <footer>
+        {" "}
+        This project created by Mona Nadi and is {""}
+        <a href="https://github.com/luna87/react-weather-app" target="_blank">
+          open-sourced on Github.
+        </a>
+      </footer>
     </div>
   );
 }
