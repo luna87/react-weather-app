a9cc8059d9427dcc442ec327cc433d0beb54cc97
mona <mo.na.di.87.de@gmail.com>
2023-05-22 19:22:53 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather app</h1>
-      <footer>
-        {" "}
-        This project created by Mona Nadi and is {""}
-        <a href="https://github.com/luna87/react-weather-app" target="_blank">
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather app</h1>
+        <button className="btn btn-primary">hello</button>
+        <footer>
+          {" "}
+          This project created by Mona Nadi and is {""}
+          <a
+            href="https://github.com/luna87/react-weather-app"
+            target="_blank"
+            rel="noopener"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
